41f2f03efb5d275fe319e3c04b301d216814a473
Salmen Hammami <hammamisalmen21@gmail.com>
2026-09-19 22:31:56 +0100

    Fix greeting message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps wor");
+  res.send("Hello DevOps world !");
 });
 
 // Connexion au serveur
